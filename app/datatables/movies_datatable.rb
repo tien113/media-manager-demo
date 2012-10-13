4be9584cd18ba16d95994c3875ba3742f86d0c79
vo <tien113@gmail.com>
2012-10-14 00:45:41 +0300

    check nil.

--- app/datatables/movies_datatable.rb
+++ app/datatables/movies_datatable.rb
@@ -42,7 +42,7 @@ private
     movies = Movie.order("#{sort_column} #{sort_direction}")
     movies = movies.page(page).per_page(per_page)
     if params[:sSearch].present?
-      movies = movies.where("title like :search or genre like :search", search: "%#{params[:sSearch]}%")
+      movies = movies.where("title like :search or genre like :search or storage like :searh", search: "%#{params[:sSearch]}%")
     end
     movies   
   end
