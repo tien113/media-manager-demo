21994ee5bcc638b78538ca3370d629712e22e787
vo <tien113@gmail.com>
2012-10-19 23:17:39 +0300

    autocomplete off, fix fetch.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -54,22 +54,27 @@ class MoviesController < ApplicationController
   private
     def fetch
       imdb_url = params[:imdb_url]
-      if imdb_url != nil
-        imdb_url = URI.parse(imdb_url)
-        if imdb_url.path.size == 17
-          imdb_id = imdb_url.path[9,7]
-          @imovie = Imdb::Movie.new(imdb_id)
-          @movie.title     = @imovie.title
-          @movie.year      = @imovie.year
-          @movie.rating    = @imovie.rating
-          @movie.genre     = @imovie.genres.join(", ")
-          @movie.imdb_url  = @imovie.url
-          @movie.image_url = @imovie.poster
-        else
-          flash.now[:error] = 'IMDb URL is wrong!'
-          @movie.year      = nil
-        end
+      if validate_imdb_url?(imdb_url)
+        @imovie = Imdb::Movie.new(imdb_id(imdb_url))
+        @movie.title     = @imovie.title
+        @movie.year      = @imovie.year
+        @movie.rating    = @imovie.rating
+        @movie.genre     = @imovie.genres.join(", ")
+        @movie.imdb_url  = @imovie.url
+        @movie.image_url = @imovie.poster
       end
+    end
+    
+    def validate_imdb_url?(url)
+      if url != nil && url.include?('http://www.imdb.com/title/tt') && url.size == 36
+        return true
+      else
+        return false
+      end
+    end
+    
+    def imdb_id(url)
+      url[28,7]
     end
   
 end
